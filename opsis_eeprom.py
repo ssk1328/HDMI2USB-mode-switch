19dbecd4455edbc2527d86cd5ab341a5dc36ec6a
Tim 'mithro' Ansell <mithro@mithis.com>
2016-01-01 20:28:56 +1100

    The first byte of MAC address doesn't need to be 0.

--- opsis_eeprom.py
+++ opsis_eeprom.py
@@ -152,7 +152,6 @@ class OpsisEEPROM(ctypes.LittleEndianStructure):
         else:
             assert self.wp_mac[0] == -1
             assert self.wp_mac[1] == -1
-            assert self.wp_mac[2] == 0
             mac = self.wp_mac[2:4] + [0xff, 0xfe] + self.wp_mac[5:]
 
         return "".join("%02x" % (x & 0xff,) for x in mac)
