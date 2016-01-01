3ee23d04558f8d938d4e6ee5e2774949225fa093
Tim 'mithro' Ansell <mithro@mithis.com>
2016-01-01 20:38:18 +1100

    Missed an assert.

--- opsis_eeprom.py
+++ opsis_eeprom.py
@@ -136,7 +136,6 @@ class OpsisEEPROM(ctypes.LittleEndianStructure):
     def eui48(self):
         assert self.wp_mac[0] == -1
         assert self.wp_mac[1] == -1
-        assert self.wp_mac[2] == 0
         return list((x & 0xff,) for x in self.wp_mac[2:])
 
     def mac(self):
